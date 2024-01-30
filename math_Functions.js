373569e04835f425800db189c35c252e88cdd5d9
VKabil <kabilvkabil@gmail.com>
2024-01-30 17:03:37 +0530

    math functions add

---
+++ math_Functions.js
@@ -0,0 +1,23 @@
+let c;
+
+c = Math.PI;
+c = Math.E;
+c = Math.round(12.2)// 12
+c = Math.round(12.8)// 13
+c = Math.floor(12.8)// 12
+c = Math.ceil(12.2) // 13
+c = Math.sqrt(20)// square root 
+c = Math.abs(-12)// absolute value in 12
+c = Math.trunc(2.85)// round value 2
+c = Math.pow(2,4)// 2^4 = 16
+c = Math.min(12,34,66,3,22,1) //Minimum value
+c = Math.max(12,34,66,3,22,1) //Maximum value
+c = Math.random()//random number
+c = Math.floor((Math.random()*100+1))// 1 to 100 random numbers
+c = Math.sign(0)
+c = Math.sin(45)
+c = Math.tan(12)
+c = Math.log(12)
+
+
+console.log(c)
